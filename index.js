003f1eab5a17e58ed2fe7115344e204ee67e3aa7
abdul mannan ullah khan <abdulmannanullahkhan115@gmail.com>
2024-06-05 01:00:56 +0500

    created number guessing game

---
+++ index.js
@@ -0,0 +1,15 @@
+import inquirer from 'inquirer';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+let Answer = await inquirer.prompt([
+    {
+        name: "guess",
+        type: 'number',
+        message: "what is you guess number between 1 and 5:"
+    }
+]);
+if (randomNumber == Answer.guess) {
+    console.log('your guess a correct number');
+}
+else {
+    console.log('sorry your guess number is wrong');
+}
