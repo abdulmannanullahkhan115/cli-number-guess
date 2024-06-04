90373c3f60c076a39eaf7296ee458c1ee90f7d6b
abdul mannan ullah khan <abdulmannanullahkhan115@gmail.com>
2024-06-05 01:08:59 +0500

    mini change

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import inquirer from 'inquirer';
 
 let randomNumber=Math.floor(Math.random() * 3+1);
-
+console.log("welcome to number guessing game")
 let Answer=await inquirer.prompt(
 [
     {
@@ -9,7 +9,7 @@ let Answer=await inquirer.prompt(
       type:'number',
       message:"what is you guess number between 1 and 5:"
 }
-       ]                                                      )
+       ]     )
 if (randomNumber==Answer.guess) {
     console.log('your guess a correct number')
 } else {
